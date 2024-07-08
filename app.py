334a9450c54e02cf8b1932884dc23f6d9d44ea9c
dillondipetrillo <dillonrdipetrillo@gmail.com>
2024-07-07 22:30:19 -0400

    add login page and ability to login

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, redirect, render_template, request, session
 from flask_session import Session
-from helpers import create_tables, get_user_id, get_username, insert_user_db, login_required, verify_sign_up_data
+from helpers import create_tables, get_user_id, get_username, insert_user_db, login_required, verify_sign_up_data, verify_login_data
 from werkzeug.security import generate_password_hash
 
 # Configure application
@@ -21,6 +21,7 @@ def index():
     username = get_username(session["user_id"])
     return render_template("index.html", username=username[0])
 
+
 @app.route("/sign-up", methods=["GET", "POST"])
 def signup():
     """Register a new user"""
@@ -33,7 +34,7 @@ def signup():
             'username': request.form.get("username"),
             'email': request.form.get("email"),
             'password': request.form.get("password"),
-            'confirm_password': request.form.get("password-confirm")
+            'confirm_password': request.form.get("password-confirm"),
         }
         # Set error message if cannot verify sign up form info
         error = verify_sign_up_data(sign_up_data)
@@ -53,4 +54,27 @@ def signup():
         return redirect('/')
     else:
         return render_template("sign-up.html")
-        
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Route for login page"""
+    # Forget any user_id
+    session.clear()
+    
+    # User is attempting to login
+    if request.method == "POST":
+        error = None
+        sign_in_data = {
+            'email': request.form.get("email"),
+            'password': request.form.get("password"),
+        }
+        # Set error message if data cannot validate
+        error = verify_login_data(sign_in_data)
+        if error:
+            return render_template("login.html", error=error)
+        return redirect('/')
+    
+    # User reached via GET (clicking a link or via redirect)
+    else:
+        return render_template("login.html")
