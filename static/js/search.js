d6cc364bac03f2b92ea7d24b4185da6ec4af2a95
dillondipetrillo <dillonrdipetrillo@gmail.com>
2024-07-13 16:52:17 -0400

    display error in search bar results container

--- static/js/search.js
+++ static/js/search.js
@@ -1,7 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
     const searchbar = document.getElementById("q");
+    const searchResultsContainer = document.getElementById("search-form-result");
 
     const fetchSearchResult = (query) => {
+        // Don't show results if search bar isn't focused
+        if (document.activeElement !== searchbar) return;
         fetch(`/results?q=${encodeURIComponent(query)}`)
         .then(response => {
             if (response.ok)
@@ -12,11 +15,13 @@ document.addEventListener("DOMContentLoaded", function () {
             if (data.Search) {
                 result = data.Search;
             } else if (data.Error === "Too many results.") {
-                result = `${data.Error} Please be more specific.`;
+                result = {
+                    Error: `${data.Error} Please be more specific.`,
+                }
             } else {
-                result = data.Error;
+                result = { Error: data.Error, };
             }
-            console.log(result);
+            buildSearchBarResults(result);
         })
         .catch(error => {
             console.log(error);
@@ -38,6 +43,43 @@ document.addEventListener("DOMContentLoaded", function () {
         }, 1250);
     })
 
+    searchbar.addEventListener("focus", () => {
+        if (searchbar.value)
+            fetchSearchResult(searchbar.value);
+    })
+
+    /* Use when going to /search page from GET request
+        Display search results in the main tag on page */
     if (searchbar.value)
         fetchSearchResult(searchbar.value);
+
+    /**
+     * Builds the search bar results
+     * @param searchResults the reults from fetch call to OMDB
+     */
+    const buildSearchBarResults = results => {
+        clearSearchResults();
+        searchResultsContainer.classList.add("border");
+
+        // Start build of search result elements
+        if (results.Error) {
+            console.log(results.Error);
+            const p = document.createElement('p');
+            p.textContent = results.Error;
+            p.classList.add("mt-3");
+            searchResultsContainer.append(p);
+        } else
+            console.log("SUCCESS", results);
+    }
+
+    const clearSearchResults = () => {
+        searchResultsContainer.innerHTML = '';
+        searchResultsContainer.classList.remove("border");
+    }
+
+    // Clear the search results container in search bar is unfocused
+    searchbar.addEventListener("focusout", () => {
+        if (!searchResultsContainer.contains(document.activeElement) && document.activeElement !== searchbar)
+            clearSearchResults();
+    })
 })
